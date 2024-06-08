fda734c8fa148663cef185a52c81a2b26f0db60b
YngPichit <161191382+YngPichit@users.noreply.github.com>
2024-06-08 19:16:09 +0700

    first add all files of project

---
+++ app.js
@@ -0,0 +1,24 @@
+//const express = require('express');
+//const chalk = require('chalk');
+import express from 'express';
+import chalk from 'chalk';
+import createDebug from 'debug';
+const debug = createDebug('app');
+// const debug = require('debug')('app');
+import morgan from 'morgan';
+// const morgan = require('morgan')
+const app = express();
+const port = 3000;
+app.use(morgan('combined'));
+
+app.get("/",(req,res)=>{
+
+    res.send("Hello everybody Yea");
+
+})
+app.listen(port,()=>{
+
+    console.log ("Listening on port" + chalk.blue(" : ",port));
+    debug ("Listening on port" + chalk.blue(" : ",port));
+
+})
